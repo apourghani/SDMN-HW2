8114b6764efcc46d943b82b37ebead3840751db4
apourghani <apourghani@gmail.com>
2022-06-07 05:01:04 -0700

    Network created

--- P1/p1_1.sh
+++ P1/p1_1.sh
@@ -6,13 +6,9 @@ sudo ip netns add NS4
 sudo ip netns add Router1
 
 
-
-
 #Creating bridges:
 sudo ip link add br1 type bridge
 sudo ip link add br2 type bridge
-
-
 
 
 #Creating links:
@@ -24,18 +20,12 @@ sudo ip link add veth5 type veth peer name veth5-br
 sudo ip link add veth6 type veth peer name veth6-br
 
 
-
 #Setting namespaces of the interfaces:
 sudo ip link set veth1 netns NS1
-
 sudo ip link set veth2 netns NS2
-
 sudo ip link set veth3 netns Router1
-
 sudo ip link set veth4 netns Router1
-
 sudo ip link set veth5 netns NS3
-
 sudo ip link set veth6 netns NS4
 
 
@@ -48,9 +38,6 @@ sudo ip link set veth5-br master br2
 sudo ip link set veth6-br master br2
 
 
-
-
-
 #Assigning ip addresses:
 sudo ip netns exec Router1 ip addr add 172.0.0.1/24 dev veth3
 sudo ip netns exec NS1 ip addr add 172.0.0.2/24 dev veth1
@@ -58,3 +45,33 @@ sudo ip netns exec NS2 ip addr add 172.0.0.3/24 dev veth2
 sudo ip netns exec Router1 ip addr add 10.10.0.1/24 dev veth4
 sudo ip netns exec NS3 ip addr add 10.10.0.2/24 dev veth5
 sudo ip netns exec NS4 ip addr add 10.10.0.3/24 dev veth6
+
+
+#Setting bridges and veths up:
+sudo ip link set br1 up
+sudo ip link set br2 up
+
+sudo ip link set veth1-br up
+sudo ip link set veth2-br up
+sudo ip link set veth3-br up
+sudo ip link set veth4-br up
+sudo ip link set veth5-br up
+sudo ip link set veth6-br up
+
+sudo ip netns exec NS1 ip link set veth1 up
+sudo ip netns exec NS2 ip link set veth2 up
+sudo ip netns exec NS3 ip link set veth5 up
+sudo ip netns exec NS4 ip link set veth6 up
+sudo ip netns exec Router1 ip link set veth3 up
+sudo ip netns exec Router1 ip link set veth4 up
+
+
+
+#Adding Routes:
+sudo ip netns exec NS1 ip route add 0.0.0.0/0 via 172.0.0.1
+sudo ip netns exec NS2 ip route add 0.0.0.0/0 via 172.0.0.1
+sudo ip netns exec NS3 ip route add 0.0.0.0/0 via 10.10.0.1
+sudo ip netns exec NS4 ip route add 0.0.0.0/0 via 10.10.0.1
+
+
+
