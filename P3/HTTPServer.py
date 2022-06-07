bc1fea4e4598d8c6ec97782ca74d401b21079ac9
apourghani <apourghani@gmail.com>
2022-06-07 11:05:58 -0700

    Bash edited

---
+++ P3/HTTPServer.py
@@ -0,0 +1,33 @@
+from http.sever import HTTPServer, BaseHTTPRequestHandler
+
+HOST = "127.0.0.1"
+PORT = 8000
+
+
+class SimpleHTTPServer(BaseHTTPRequestHandler):
+	GETOK = True
+	def do_GET(self):
+		if self.path == '/api/v1/status':
+			if self.GETOK:
+				self.send_response(200)
+				self.send_header("Content-type", "application/json")
+				self.end_headers()
+				self.wfile.write(bytes('{"status": "OK"}'), "utf-8"))
+			else:
+				self.send_response(201)
+				self.send_header("Content-type", "application/json")
+				self.end_headers()
+				self.wfile.write(bytes('{"status": "not OK"}'), "utf-8"))
+				
+
+def do_POST(self):
+		if self.path == '/api/v1/status':
+			self.send_response(200)
+			self.send_header("Content-type", "application/json")
+			self.end_headers()
+			self.wfile.write(bytes('{"status": "OK"}'), "utf-8"))
+
+
+
+server = HTTServer((HOST, PORT), SimpleHTTPServer)
+
