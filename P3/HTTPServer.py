ad64f2a70818c1e3a61c27dc2c1c58c79a4511ac
apourghani <apourghani@gmail.com>
2022-06-07 23:43:53 -0700

    POST function defined, but not completed

--- P3/HTTPServer.py
+++ P3/HTTPServer.py
@@ -1,34 +1,75 @@
-from http.sever import HTTPServer, BaseHTTPRequestHandler
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import http.server
+import cgi
 
-HOST = "127.0.0.1"
-PORT = 8000
+
 
 
 class SimpleHTTPServer(BaseHTTPRequestHandler):
 	GETOK = True
 	def do_GET(self):
+
 		if self.path == '/api/v1/status':
 			if self.GETOK:
 				self.send_response(200)
 				self.send_header("Content-type", "application/json")
 				self.end_headers()
-				self.wfile.write(bytes('{"status": "OK"}'), "utf-8"))
+				self.wfile.write(bytes('{"status": "OK"}'), "utf-8")
 			else:
+				self.send_response(200)
+				self.send_header("Content-type", "application/json")
+				self.end_headers()
+				self.wfile.write(bytes('{"status": "not OK"}'), "utf-8")
+
+
+	def do_POST(self):
+		if self.path == '/api/v1/status':
+			contentType, paramDict = cgi.parse_header(self.headers.get("Content-type"))
+			paramDict["boundary"] = bytes(paramDict["boundary"], "utf-8") 		#Doubt???????????
+			recContent = cgi.parse_multipart(self.rfile, paramDict)
+			if recContent.get("status") == "not OK":
 				self.send_response(201)
 				self.send_header("Content-type", "application/json")
 				self.end_headers()
-				self.wfile.write(bytes('{"status": "not OK"}'), "utf-8"))
+				self.wfile.write(bytes('{"status": "not OK"}'), "utf-8")
+				self.GETOK = False
 				
-
-def do_POST(self):
-		if self.path == '/api/v1/status':
-			self.send_response(200)
-			self.send_header("Content-type", "application/json")
-			self.end_headers()
-			self.wfile.write(bytes('{"status": "OK"}'), "utf-8"))
-
+			elif recContent.get("status") == "OK" and self.GETOK:
+				self.send_response(201)
+				self.send_header("Content-type", "application/json")
+				self.end_headers()
+				self.wfile.write(bytes('{"status": "not OK"}'), "utf-8")
+				self.GETOK = True
 
 
-server = HTTServer((HOST, PORT), SimpleHTTPServer)
+
+
+def main():
+	HOST = "localhost"
+	PORT = 8000
+	server_ip_port = (HOST, PORT) 
+	server = HTTPServer(server_ip_port, SimpleHTTPServer)
+	print("Server is running ....")
+	server.serve_forever()
+	
+	
+
+
+
+if __name__ == '__main__':
+	main()
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
