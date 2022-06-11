36886f14aa8d963afc7d4303bb45fddeef69024d
apourghani <apourghani@gmail.com>
2022-06-11 10:56:17 -0700

    P2 completed, P2s screenshot added, P1 answers added, and some minor edits

--- P2/CLIRun.py
+++ P2/CLIRun.py
@@ -4,11 +4,12 @@ args_num = len(sys.argv)
 
 #When just hostname specified:
 if args_num == 2:
+
 	print(sys.argv[1])
 	os.system(f"python3 ./CLIone.py {sys.argv[1]}")
 #When hostname and memory specified:
 elif args_num == 3:
-	os.system(f"python3 ./CLItwo.py {sys.argv[2]}")
+	os.system(f"python3 ./CLIone.py {sys.argv[1]} {sys.argv[2]}")
 
 
 #Wrong number of inputs:
--- P2/CLIone.py
+++ P2/CLIone.py
@@ -3,30 +3,14 @@ import sys
 
 
 def startContainer():
-	print("start")
+	
+	os.system('mkdir aminsbox')
+	
 	hostname = sys.argv[1]
-	os.system('mkdir /aminbox')
-	
-	os.system('sudo apt install --assume-yes debootstrap')
-	
-	os.system('sudo debootstrap focal ~/aminbox')
-	
-	os.system('sudo mount -t proc /proc ~/aminbox/proc')
-	
-	os.system('sudo mount --rbind /sys ~/aminbox/sys')
-	
-	os.system('sudo mount --rbind /dev ~/aminbox/dev')
-	
-	os.system(f"unshare --net --mount --pid --uts --fork --map-root-user --mount-proc")
-	
-	os.system(str('sudo hostname '+hostname))
-	
-	os.system('sudo chroot ~/aminbox /bin/bash')
-	
-	
+		
+	os.system('tar -zxf ubuntu-focal-oci-amd64-root.tar.gz -C aminsbox')
 
-	
-	
+	os.system("sudo unshare --fork --pid --mount-proc --uts --net --root=/aminsbox bash -c  \"hostname "+hostname+" && /bin/bash\"")
 	
 	
 if __name__ == '__main__':		
